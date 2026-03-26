ba1b049aff7ebd8294cf94eab2baa3412ae6ce24
Nalintorn Sopapiriyatorn <nalintorn.s@ku.th>
2026-03-26 14:46:41 +0000

    Update logo and text in App component for CI/CD pipeline

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,11 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src="Octocat.png" className="App-logo" alt="logo" />
+        <img src="KU_SubLogo.png" className="App-logo" alt="logo" />
         <p>
-          GitHub Codespaces <span className="heart">♥️</span> React
+          CI/CD Pipeline for React Web app <span className="heart">♥️</span> 
         </p>
         <p className="small">
-          Edit <code>src/App.jsx</code> and save to reload.
         </p>
         <p>
           <a
